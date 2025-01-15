135a82ff8c28ae73def33f00903e2ee43c225840
Volodymyr <vivk2@ukr.net>
2025-01-15 19:46:07 +0200

    Add files via upload

---
+++ mywebdemo/src/main/java/com/myweb/mywebdemo/MywebdemoApplication.java
@@ -0,0 +1,13 @@
+package com.myweb.mywebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebdemoApplication.class, args);
+	}
+
+}
---
+++ mywebdemo/src/test/java/com/myweb/mywebdemo/MywebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myweb.mywebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
